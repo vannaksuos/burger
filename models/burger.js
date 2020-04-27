d4b75ebad8c28c0947e505d939601b002a33eb29
Vannak <vannaksuos@gmail.com>
2020-04-27 12:33:40 -0400

    make it better

--- models/burger.js
+++ models/burger.js
@@ -2,19 +2,19 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function (cb) {
-        orm.all("cats", function (res) {
+    selectAll: function (cb) {
+        orm.selectAll("burger", function (res) {
             cb(res);
         });
     },
     // The variables cols and vals are arrays.
     create: function (cols, vals, cb) {
-        orm.create("burger", cols, vals, function (res) {
+        orm.insertOne("burger", cols, vals, function (res) {
             cb(res);
         });
     },
     update: function (objColVals, condition, cb) {
-        orm.update("burger", objColVals, condition, function (res) {
+        orm.updateOne("burger", objColVals, condition, function (res) {
             cb(res);
         });
     },
