aac5a2c839e2f12f4edd6dbd4cd899767aa85aaa
Vannak <vannaksuos@gmail.com>
2020-04-27 13:50:56 -0400

    trying to fix index to index.handlebarls

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,12 +6,12 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
+    burger.All(function (data) {
         var hbsObject = {
             burgers: data
         };
         console.log(hbsObject);
-        res.render("index.handlebars", hbsObject);
+        res.render("index", hbsObject);
     });
 });
 
@@ -45,18 +45,18 @@ router.put("/api/burgers/:id", function (req, res) {
     });
 });
 
-router.delete("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
+// router.delete("/api/burgers/:id", function (req, res) {
+//     var condition = "id = " + req.params.id;
 
-    burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
+//     burger.delete(condition, function (result) {
+//         if (result.affectedRows == 0) {
+//             // If no rows were changed, then the ID must not exist, so 404
+//             return res.status(404).end();
+//         } else {
+//             res.status(200).end();
+//         }
+//     });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
