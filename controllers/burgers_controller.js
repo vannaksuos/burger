15f794a787c56c98dd72c11a93e4ee70467cf2f4
Vannak <vannaksuos@gmail.com>
2020-04-27 13:38:04 -0400

    trying to fix index to index.handlebarls

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
             burgers: data
         };
         console.log(hbsObject);
-        res.render("index", hbsObject);
+        res.render("index.handlebars", hbsObject);
     });
 });
 
