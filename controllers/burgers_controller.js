774fd006d23b4f63c9348ea7a31b256021fc99b0
Vannak <vannaksuos@gmail.com>
2020-04-27 12:39:11 -0400

    make it better

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var router = express.Router();
-var burger = require("../models/burger.js");
 
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    burger.all(function (data) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
